24abd616945a5f8f89d202c1a3a6f64b6a3b17bf
christiano112 <enyiachristian12@gmail.com>
2024-01-25 22:25:38 +0100

    init project

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -4,8 +4,7 @@ export default function Root(props) {
   const myName = "Christiano";
   return (
     <section>
-      {props.name} is mounted!
-      Home Page
+      {props.name} is mounted! Home Page
       <Emmy name={myName} />
     </section>
   );
